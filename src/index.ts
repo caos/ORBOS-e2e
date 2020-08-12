a7cb5784c4a2d753c28ecc9327b24be9a66b02fa
Elio Bischof <eliobischof@gmail.com>
2020-08-12 17:52:12 +0200

    fix repo url

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ async function run(): Promise<void> {
 
   try {
 
-    await exec.exec("git", ["clone", `https://${core.getInput("personal-access-token")}@github.com/${github.context.repo.owner}/${github.context.repo.owner}.git`])
+    await exec.exec("git", ["clone", `https://${core.getInput("personal-access-token")}@github.com/${github.context.repo.owner}/${github.context.repo.repo}.git`])
 
   } catch (error) {
     core.setFailed(error.message)
