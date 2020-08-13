a9804d1337fdd415d3a6ad4259675cd38491d42c
Elio Bischof <eliobischof@gmail.com>
2020-08-13 10:59:20 +0200

    commit transpiled code

--- dist/index.js
+++ dist/index.js
@@ -21,10 +21,19 @@ var __importStar = (this && this.__importStar) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const core = __importStar(require("@actions/core"));
 const github = __importStar(require("@actions/github"));
-const exec = __importStar(require("@actions/exec"));
+const shell = __importStar(require("shelljs"));
 async function run() {
     try {
-        await exec.exec("git", ["clone", `https://${core.getInput("personal-access-token")}@github.com/${github.context.repo.owner}/${github.context.repo.repo}.git`]);
+        shell.rm("-rf", github.context.repo.repo);
+        shell.exec(`git clone https://${core.getInput("github-token")}@github.com/${github.context.repo.owner}/${github.context.repo.repo}.git`);
+        shell.cd(github.context.repo.repo);
+        shell.exec("git config user.name github-actions");
+        shell.exec("git config user.email github-actions@github.com");
+        shell.exec(`git commit --allow-empty -m "empty commit"`);
+        shell.exec("git push");
+        shell.rm("-rf", "ORBOS");
+        shell.exec(`git clone https://${core.getInput("github-token")}@github.com/caos/ORBOS.git`);
+        shell.cd("ORBOS");
     }
     catch (error) {
         core.setFailed(error.message);
