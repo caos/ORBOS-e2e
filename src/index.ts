1ce32df5c2669c749a50a0dc56c57808f80b2cb6
Elio Bischof <eliobischof@gmail.com>
2020-08-13 11:16:12 +0200

    handle errors

--- src/index.ts
+++ src/index.ts
@@ -10,22 +10,40 @@ async function run(): Promise<void> {
   try {
 
     shell.rm("-rf", github.context.repo.repo)
+    trowErr()
     shell.exec(`git clone https://${core.getInput("github-token")}@github.com/${github.context.repo.owner}/${github.context.repo.repo}.git`)
+    trowErr()
     shell.cd(github.context.repo.repo)
+    trowErr()
 
-    shell.exec("git config user.name github-actions")	
+    shell.exec("git config user.name github-actions")
+    trowErr()
     shell.exec("git config user.email github-actions@github.com")
+    trowErr()
     shell.exec(`git commit --allow-empty -m "empty commit"`)
+    trowErr()
     shell.exec("git push")
+    trowErr()
 
     shell.rm("-rf", "ORBOS")
+    trowErr()
     shell.exec(`git clone https://${core.getInput("github-token")}@github.com/caos/ORBOS.git`)
+    trowErr()
     shell.cd("ORBOS")
+    trowErr()
 
   } catch (error) {
     core.setFailed(error.message)
   }
 }
+
+function trowErr(){
+  let err = shell.error() 
+  if (err){
+    throw err
+  }
+}
+
 
 async function cleanup(): Promise<void> {
 
